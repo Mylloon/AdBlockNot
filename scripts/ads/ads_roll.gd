8b9fc4a52407b0a1e368c174eb6709dcb035fa65
Mylloon <kennel.anri@tutanota.com>
2024-01-28 18:27:12 +0100

    rolls

--- scripts/ads/ads_roll.gd
+++ scripts/ads/ads_roll.gd
@@ -1,9 +1,21 @@
 extends "res://scripts/ads/ads_default.gd"
 
-# TODO
+@onready var button      := $ButtonDice/Button
+@onready var label_left  := $Dice/Left
+@onready var label_right := $Dice/Right
 
 func _ready():
-	pass
+	label_left.text  = "0"
+	label_right.text = "0"
 
 func exit_condition():
 	return condition
+
+func _on_button_pressed():
+	var left  := randi_range(0, 6)
+	var right := randi_range(0, 6)
+	
+	label_left.text  = str(left)
+	label_right.text = str(right)
+	
+	condition = left + right >= 5
