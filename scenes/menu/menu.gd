c7d6a5c17a0704b0e9a9a2a533c0898682e8651e
2-1-1-2 <tanyan.francois@gmail.com>
2024-01-28 21:53:02 +0100

    Mode infini et normal implémenté + écran de fin (victoire & défaite)

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Menu/Options/Rules.set_visible(false)
@@ -16,10 +15,12 @@ func _on_power_off_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu/leaving_scene.tscn")
 
 func _on_normal_start_pressed():
-	#get_tree().change_scene_to_file("")
+	Global.mode = 1
+	get_tree().change_scene_to_file("res://scenes/game/screen.tscn")
 	pass # Replace with function body.
 
 func _on_infinite_start_pressed():
+	Global.mode = 2
 	get_tree().change_scene_to_file("res://scenes/game/screen.tscn")
 
 func _on_options_pressed():
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,9 @@
 extends Control
 var ads_scene = []
 var ads = []
+var level = [10, 15, 25]
+var nlevel = 0
+var nb = 10
 """
 var ads_scene = [preload("res://scenes/game/ads/Ads_password.tscn"), 
 preload("res://scenes/game/ads/Ads_sudoku.tscn")]
@@ -33,6 +36,7 @@ func new_ads():
 	print("her")
 	randomize()
 	var _range = randi_range(0, ads_scene.size()-1)
+	print(_range)
 	var instance = ads_scene[_range].instantiate()
 	add_child(instance) # Replace with function body.
 	var _scale = Vector2(0.5,0.5)
@@ -46,34 +50,69 @@ func new_ads():
 
 # Called when the node enters the scene tree for the first time.
 func load_scene():
-	for scene in [ "res://scenes/game/ads/ads_music.tscn"
-				 , "res://scenes/game/ads/ads_normal.tscn"
-				 , "res://scenes/game/ads/ads_operation.tscn"
-				 , "res://scenes/game/ads/ads_pair.tscn"
-				 , "res://scenes/game/ads/ads_roll.tscn"
-				 , "res://scenes/game/ads/ads_sexy_robot.tscn"
-				 , "res://scenes/game/ads/ads_weather.tscn" ]:
+	var _ads_scene = getFilePathsByExtension("res://scenes/game/ads", "tscn")
+	for scene in _ads_scene:
 		ads_scene.push_back(load(scene))
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		pass # todo one day ahah ^^
 		# $Rules.set_visible(true)
+		
+func newLevel():
+	for n in nb:
+		new_ads()
+	Reset_Timer()
+	$Timer.start()
 
 func _ready():
 	load_scene()
 	print(ads_scene)
-	for n in 15:
-		new_ads()
+	
+	if Global.mode == 1 :
+		nb = level[nlevel]
+	
+	if Global.mode == 2 :
+		pass
+	newLevel()
+	
+var seconds=0
+var Dsecond=45
+
+
 	
 func win():
 	for ad in ads:
 		if ad.visible == true :
 			return false
+	if Global.mode == 1 :
+		nlevel +=1
+		if nlevel < 3:
+			nb = level[nlevel]
+			newLevel()
+			return false
 	return true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func lose():
+	return seconds == 0
+
 func _process(_delta):
+	if lose():
+		$Timer.stop()
+		get_tree().change_scene_to_file("res://scenes/game/defeat.tscn")
 	if win():
-		for n in 15:
-			new_ads()
-		# get_tree().change_scene_to_file("res://scenes/game/victoire.tscn")
+		if Global.mode == 1 :
+			$Timer.stop()
+			get_tree().change_scene_to_file("res://scenes/game/victory.tscn")
+		if Global.mode == 2 :
+			nb +=5
+			newLevel()
+
+
+func _on_timer_timeout():
+	$Label.text = str(seconds)
+	seconds -=1
+	
+func Reset_Timer():
+	seconds=Dsecond
+	
