01110ed3c1f443a4d829e314cfef4e7ce10dfc79
2-1-1-2 <tanyan.francois@gmail.com>
2024-01-28 02:34:46 +0100

    mise en place du gameplay

---
+++ scripts/ads/ads_default.gd
@@ -0,0 +1,18 @@
+extends "res://scripts/ads.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Panel2/Button.disabled = true
+	pass
+	
+
+func exit_condition():
+	return condition
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	close()
+
+func _on_button_pressed():
+	print("pressed")
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,20 +1,69 @@
 extends Control
+var ads_scene = []
+var ads = []
+"""
+var ads_scene = [preload("res://scenes/game/ads/Ads_password.tscn"), 
+preload("res://scenes/game/ads/Ads_sudoku.tscn")]
+"""
 
-var window = preload("res://scenes/game/windows/Windows.tscn")
-var instance = window.instantiate()
+func getFilePathsByExtension(directoryPath: String, extension: String) -> Array:
+	var dir =  DirAccess.open(directoryPath)
+	if !dir :
+		printerr("Warning: could not open directory: ", directoryPath)
+		return []
+	
+	var filePaths := []
+	if dir:
+		dir.list_dir_begin()
+		var fileName = dir.get_next()
+		while fileName != "":
+			if dir.current_is_dir():
+				print("Found directory: " + fileName)
+			else:
+				print("Found file: " + fileName)
+				if fileName.get_extension() == extension:
+					var filePath = dir.get_current_dir() + "/" + fileName
+					filePaths.append(filePath)
+			fileName = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	return filePaths
 
 func new_ads():
-	instance.position = Vector2(10.0, 5.0)
-	var _size = instance.get_rect()
-	print(_size)
+	print("her")
+	randomize()
+	var _range = randi_range(0, ads_scene.size()-1)
+	var instance = ads_scene[_range].instantiate()
 	add_child(instance) # Replace with function body.
+	var _scale = Vector2(0.5,0.5)
+	instance.scale = _scale
+	var _size = instance.get_size() * _scale
+	print(_size)
+	instance.position.x = randf_range(1, get_viewport_rect().size.x - (_size.x))
+	instance.position.y = randf_range(1, get_viewport_rect().size.y - (_size.y*1.5))
+	ads.push_back(instance)
+	print(instance.position)
 
 # Called when the node enters the scene tree for the first time.
+func load_scene():
+	var _ads_scene = getFilePathsByExtension("res://scenes/game/ads", "tscn")
+	for scene in _ads_scene:
+		ads_scene.push_back(load(scene))
+
+
 func _ready():
-	new_ads()
+	load_scene()
+	print(ads_scene)
+	for n in 15:
+		new_ads()
 	
-
-
+func win():
+	for ad in ads:
+		if ad.visible == true :
+			return false
+	return true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if win():
+		pass
+		print("mettre écran victoire")
